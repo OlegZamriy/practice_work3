f56c8acbe3582546ed1c2fbdcc889f2abaa83eb8
OlegZamriy <mynameishelg@gmail.com>
2023-09-19 19:51:31 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
+file_size_gb = float(input("Введіть розмір файлу в гігабайтах: "))
 
+internet_speed_bps = float(input("Введіть швидкість інтернет-з'єднання в бітах на секунду: "))
 
+choice = input("Оберіть 'години', 'хвилини' або 'секунди' для обчислення часу завантаження (введіть '1' або '2' або '3'): ")
 
-price_per_console = float(input("Введіть вартість однієї ігрової приставки: "))
-quantity = int(input("Введіть кількість ігрових приставок: "))
-discount_percentage = float(input("Введіть відсоток знижки: "))
-choice = input("Оберіть 'загальна сума' або 'вартість однієї приставки' (введіть '1' або '2'): ")
+file_size_bits = file_size_gb * 8 * 1024 * 1024 * 1024
+
+download_time_seconds = file_size_bits / internet_speed_bps
 
 if choice == '1':
-    total_price = price_per_console * quantity * (1 - discount_percentage / 100)
-    print(f"Загальна сума замовлення з урахуванням знижки: {total_price:.2f} грн.")
+    download_time = download_time_seconds / 3600
+    print(f"Час завантаження файлу: {download_time:.2f} годин.")
 elif choice == '2':
-    discounted_price = price_per_console * (1 - discount_percentage / 100)
-    print(f"Вартість однієї приставки зі знижкою: {discounted_price:.2f} грн.")
+    download_time = download_time_seconds / 60
+    print(f"Час завантаження файлу: {download_time:.2f} хвилин.")
+elif choice == '3':
+    print(f"Час завантаження файлу: {download_time_seconds:.2f} секунд.")
 else:
-    print("Неправильний вибір. Будь ласка, введіть 'загальна сума' або 'приставка'.")
+    print("Неправильний вибір. Будь ласка, введіть 'години', 'хвилини' або 'секунди'.")
+
